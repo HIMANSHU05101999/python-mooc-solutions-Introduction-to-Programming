62794ea461542fe9aa5a022a5a6d412b8a4993ae
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-02 01:03:57 +0530

    Last Exercise left in Part7 Times and dates

--- part07-09_how_old/src/how_old.py
+++ part07-09_how_old/src/how_old.py
@@ -1,1 +1,15 @@
 # Write your solution here
+from datetime import datetime
+def how_old():
+    day=int(input("Day: "))
+    month=int(input("Month:"))
+    year=int(input("Year:"))
+    dob=datetime(year,month,day)
+    diff=datetime(1999,12,31)-dob
+    if (diff)>(datetime(1999,12,31)-datetime(1999,12,31)):
+        return(f"You were {diff.days} days old on the eve of the new millennium.")
+    else:
+        return("You weren't born yet on the eve of the new millennium.")
+def main():
+    print(how_old())
+main()
--- part07-10_valid_pic/src/valid_pic.py
+++ part07-10_valid_pic/src/valid_pic.py
@@ -1,1 +1,38 @@
 # Write your solution here
+from datetime import datetime
+def is_it_valid(pic: str):
+    if len(pic)!=11:
+        return False
+    
+    control_char="0123456789ABCDEFHJKLMNPRSTUVWXY"
+    
+    cent_marker={"+":"18",
+                 "-":"19",
+                 "A":"20"
+                }
+    year=0
+    if pic[6] in cent_marker:
+        year=int(cent_marker[pic[6]]+pic[4:6])
+
+    month=int(pic[2:4])
+   
+    day=int(pic[:2])
+   
+    control_char_chk_var=int(pic[:6]+pic[7:10])%31
+    control_chk=str(control_char[control_char_chk_var])
+    
+    try:
+        if datetime(year,month,day):
+            if control_chk==(pic[-1]):
+                return True
+            else:
+                return False
+    except ValueError:    
+        return False
+    
+def main():
+    is_it_valid("230827-906F")
+    is_it_valid("120488+246L")
+    is_it_valid("310823A9877")
+if __name__=="__main__":
+    main()
