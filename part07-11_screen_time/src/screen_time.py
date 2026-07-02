750a9c18ff5a3f6d1c20ad5f0be04fa4e650eea0
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-02 19:50:52 +0530

    Completed Part7: Times Dates, Started with Data Processing, Json

--- part07-11_screen_time/src/screen_time.py
+++ part07-11_screen_time/src/screen_time.py
@@ -1,1 +1,40 @@
 # Write your solution here
+from datetime import datetime,timedelta
+
+if False:
+        file_name="abc.txt"
+        start_date="1.1.2021"
+        start_date=datetime.strptime(start_date,"%d.%m.%Y")
+        initial_date=start_date
+        num_of_days=3
+else:
+        file_name=input("Filename:")
+        start_date=input("Starting date:")
+        start_date=datetime.strptime(start_date,"%d.%m.%Y")
+        initial_date=start_date
+        num_of_days=int(input("How many days:"))
+
+print("Please type in screen time in minutes on each day (TV computer mobile):")
+    
+total_time=0
+empty_list=[]
+data_list=[]
+    
+
+for i in range(num_of_days):
+            screen_time=input(f"Screen time {start_date.strftime('%d.%m.%Y')}: ")
+            data_list.append(f"{start_date.strftime('%d.%m.%Y')}: {screen_time.replace(' ','/')}")
+            empty_list=screen_time.split()
+            #print(empty_list)
+            for item in empty_list:
+                total_time+=int(item)
+            #print(total_time)
+            start_date=start_date + timedelta(days=1)
+            #print(data_list)
+with open(file_name,"w") as file:
+        file.write(f"Time period: {initial_date.strftime('%d.%m.%Y')}-{(initial_date+timedelta(days=num_of_days-1)).strftime('%d.%m.%Y')}\nTotal minutes: {total_time}\nAverage minutes: {total_time/num_of_days}")
+        for item in data_list:
+            file.write("\n"+item)   
+print(f"Data stored in file {file_name}")  
+
+
--- part07-12_json_files/src/json_files.py
+++ part07-12_json_files/src/json_files.py
@@ -1,1 +1,39 @@
 # Write your solution here
+import json
+def print_persons(filename: str):
+    from pathlib import Path
+    file_name=Path(__file__).parent/filename
+    with open(file_name) as file:
+        data=file.read()
+    person=json.loads(data)
+    
+    new_str=""
+    for item in person:
+        for key,val in item.items():
+            if key=="name":
+                new_str+=val+" "
+            if key=="age":
+                val=str(val)+" years "
+                new_str+=val
+            if key=="hobbies":
+                if val!=[]:
+                    string=""
+                    for i in range(len(val)):
+                        if i==0:
+                            string+=str('(') 
+                        if i<len(val)-1:
+                            string+=str(val[i]+', ')    
+                        else:
+                            string+=str(val[i]+')\n')
+                    val=string  
+                    string=""
+                    new_str+=val
+                else:
+                    new_str=new_str+"()\n"
+    print(new_str[:-1])
+
+
+def main():
+    print_persons("file4.json")
+if __name__=="__main__":
+    main()
