8b78ed89f0364537a1829a05630f776b7e7c0421
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-03 23:41:36 +0530

    Completed Course Stastics Exercise

--- part07-13_course_statistics/src/course_statistics.py
+++ part07-13_course_statistics/src/course_statistics.py
@@ -1,1 +1,48 @@
 # Write your solution here
+import urllib.request
+import json
+
+def retrieve_course(course_name: str):
+    url=f"https://studies.cs.helsinki.fi/stats-mock/api/courses/{course_name}/stats"
+    url_content=urllib.request.urlopen(url)
+    url_data=url_content.read()
+    course_data=json.loads(url_data)
+    students=[]
+    hours=0
+    exercises=0
+    weeks=0
+    for _,line in course_data.items():
+        students.append(line['students'])
+        hours+=line['hour_total']
+        exercises+=line['exercise_total']
+        weeks+=1
+        
+    students=(max(students))
+    hours_average=int(hours/students)
+    exercises_average=int(exercises/students)
+    stats_dict={'weeks':weeks,'students':students,'hours':hours,'hours_average':hours_average,'exercises':exercises,'exercises_average':exercises_average}
+    return stats_dict
+    
+def retrieve_all():
+    url="https://studies.cs.helsinki.fi/stats-mock/api/courses"
+    url_content=urllib.request.urlopen(url)
+    data=url_content.read()
+    course_data=json.loads(data)
+    
+    list_of_tuples=[]
+    tup=()
+    for line in course_data:
+        if line['enabled']==True:
+            full_name=(line['fullName'])
+            name=(line['name'])
+            year=(line['year'])
+            exercises=((line['exercises']))
+            tup=(full_name,name,year,sum(exercises))
+            list_of_tuples.append(tup)
+    return list_of_tuples
+
+def main():
+    retrieve_all()
+    retrieve_course("docker2019")
+if __name__=="__main__":
+    main()
--- part07-14_who_cheated/src/who_cheated.py
+++ part07-14_who_cheated/src/who_cheated.py
@@ -1,1 +1,31 @@
 # Write your solution here
+import csv
+from pathlib import Path
+from datetime import datetime
+def cheaters():
+    start_dict={}
+    cheater_list=[]
+    start_time_file=Path(__file__).parent/"start_times.csv"
+    submission_file=Path(__file__).parent/"submissions.csv"
+    with open(start_time_file) as start_file, open(submission_file) as sub_file:
+        for line in csv.reader(start_file,delimiter=";"):
+           start_dict[line[0]]=datetime.strptime(line[1],'%H:%M')
+        for line in csv.reader(sub_file,delimiter=";"):
+            print(line)
+   
+        for key,val in start_dict.items():
+            if key in line:
+                if datetime.strptime(line[3],'%H:%M')-val>3:
+                    cheater_list.append(key)
+        print(cheater_list)
+
+
+            
+                    
+
+   
+       
+def main():
+    cheaters()
+if __name__=="__main__":
+    main()
