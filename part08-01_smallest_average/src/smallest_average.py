7f2726d519a6ae631062ec3779a85af6f9814954
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-09 22:27:41 +0530

    Completed Part8: Objects and Methods \\\  Classes and Objects

--- part08-01_smallest_average/src/smallest_average.py
+++ part08-01_smallest_average/src/smallest_average.py
@@ -1,1 +1,25 @@
 # Write your solution here
+def smallest_average(person1: dict, person2: dict, person3: dict):
+    person1_avg=(person1['result1']+person1['result2']+person1['result3'])/3
+    person2_avg=(person2['result1']+person2['result2']+person2['result3'])/3
+    person3_avg=(person3['result1']+person3['result2']+person3['result3'])/3
+
+    if person1_avg<person2_avg and person1_avg<person3_avg:
+        #print(person1_avg)
+        return person1
+    elif person2_avg<person3_avg and person2_avg<person1_avg:
+        #print(person2_avg)
+        return person2
+    else:
+        #print(person3_avg)
+        return person3
+
+
+def main():
+    person1 = {"name": "Mary", "result1": 2, "result2": 3, "result3": 3}
+    person2 = {"name": "Gary", "result1": 5, "result2": 1, "result3": 8}
+    person3 = {"name": "Larry", "result1": 3, "result2": 1, "result3": 1}
+
+    print(smallest_average(person1, person2, person3))
+if __name__=="__main__":
+    main()
--- part08-03_list_years/src/list_years.py
+++ part08-03_list_years/src/list_years.py
@@ -1,3 +1,20 @@
 # Write your solution here
 # Remember the import statement
 # from datetime import date
+from datetime import date
+def list_years(dates: list):
+    year_list=[]
+    for date in dates:
+        year_list.append(date.year)
+    return sorted(year_list)
+    
+
+def main():
+    date1 = date(2019, 2, 3)
+    date2 = date(2006, 10, 10)
+    date3 = date(1993, 5, 9)
+
+    years = list_years([date1, date2, date3])
+    print(years)
+if __name__=="__main__":
+    main()
