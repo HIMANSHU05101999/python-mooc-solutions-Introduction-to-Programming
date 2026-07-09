bcd70d646cd337892c5260ab5feb88ff17261dbc
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-09 20:59:17 +0530

    Finished Part8: Object and Methods

--- part08-02_row_sums/src/row_sums.py
+++ part08-02_row_sums/src/row_sums.py
@@ -1,1 +1,15 @@
-# Write your solution here
+# Write your solution 
+def row_sums(matrix: list):
+    
+    for row in matrix:
+        total=0
+        for num in row:
+            total+=num
+        row.append(total)
+
+def main():
+    my_matrix = [[1, 2], [3, 4]]
+    row_sums(my_matrix)
+    print(my_matrix)
+if __name__=='__main__':
+    main()
