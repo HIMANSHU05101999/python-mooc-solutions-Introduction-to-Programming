dacaf2b370dd31b55f48e60d2afad0a4ac02f5bc
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-11 05:15:15 +0530

    Part 8 Exercise 11 Completed

--- part08-11_first_and_last_name/src/first_and_last_name.py
+++ part08-11_first_and_last_name/src/first_and_last_name.py
@@ -1,13 +1,34 @@
 # Write your solution here:
+class Person:
+    def __init__(self, name: str):
+        self.name=name
+        
+    
+    def return_first_name(self):
+        cutoff_index = self.name.find(" ")
+        return self.name[0:cutoff_index]
+        
+    
+    def return_last_name(self):
+        cutoff_index = self.name.find(" ")
+        return self.name[cutoff_index+1:]
+        
 
 
 
 
+def main():
+    peter = Person("Peter Pythons")
+    print(peter.return_first_name())
+    print(peter.return_last_name())
+
+    paula = Person("Paula Pythonnen")
+    print(paula.return_first_name())
+    print(paula.return_last_name())
 
 
-
-
-
+if __name__ == "__main__": 
+    main()
 
 
 
