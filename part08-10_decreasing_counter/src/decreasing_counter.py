630fa509f796f7fdafa693a4f6970ad6d81449e6
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-11 04:49:41 +0530

    Completed Part8 Exercise 10

--- part08-10_decreasing_counter/src/decreasing_counter.py
+++ part08-10_decreasing_counter/src/decreasing_counter.py
@@ -1,12 +1,30 @@
 # Tee ratkaisusi tähän:
 class DecreasingCounter:
     def __init__(self, initial_value: int):
+        self.first_value=initial_value
         self.value = initial_value
 
     def print_value(self):
         print("value:", self.value)
 
     def decrease(self):
-        pass
-
+        if self.value>0:
+            self.value-=1
+        else:
+            self.value=0
     # Write the rest of the methods here!
+    def set_to_zero(self):
+        self.value=0
+
+    def reset_original_value(self):
+        self.value=self.first_value
+
+def main():
+    counter = DecreasingCounter(10)
+    counter.print_value()
+    counter.decrease() 
+    counter.print_value()
+    counter.decrease()
+    counter.print_value()
+if __name__=="__main__":
+    main()
