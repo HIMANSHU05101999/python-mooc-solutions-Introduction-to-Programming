e65852a4bdba4bba30e5cb962e73fd07eba13c7f
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-15 22:55:40 +0530

    Part9 Exercise11 Completed Encapsulation Completed

--- part09-11_weather_station/src/weather_station.py
+++ part09-11_weather_station/src/weather_station.py
@@ -1,3 +1,35 @@
 # WRITE YOUR SOLUTION HERE:
+class WeatherStation:
+    def __init__ (self, name: str):
+        self.__name=name
+        self.__observation=[]
 
+    def add_observation(self, observation: str):
+            self.__observation.append(observation)
+            
+    def latest_observation(self):
+        if self.__observation:
+            tot_obs=self.number_of_observations()-1
+            return self.__observation[tot_obs]
+        else:
+            return ""
 
+    def number_of_observations(self):
+        return len(self.__observation)
+
+    def __str__(self):
+        return(f"{self.__name}, {self.number_of_observations()} observations")
+
+def main():
+    station = WeatherStation("Houston")
+    station.add_observation("Rain 10mm")
+    station.add_observation("Sunny")
+    print(station.latest_observation())
+
+    station.add_observation("Thunderstorm")
+    print(station.latest_observation())
+
+    print(station.number_of_observations())
+    print(station)
+if __name__=="__main__":
+    main()
