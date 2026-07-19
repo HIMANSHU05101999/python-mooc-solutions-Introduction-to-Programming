0e94f9fd91d94c6f428392ef6aea344a51100123
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-19 11:42:39 +0530

    Part10 Exercise 1 Completed

--- part10-01_laptop_computer/src/laptop_computer.py
+++ part10-01_laptop_computer/src/laptop_computer.py
@@ -11,3 +11,20 @@ class Computer:
     @property
     def speed(self):
         return self.__speed
+    def __str__(self):
+        return (f"{self.__model}, {self.__speed} MHz")
+    
+class LaptopComputer(Computer):
+    def __init__(self, model, speed, weight):
+        super().__init__(model, speed)
+        self.__weight=weight
+
+    
+    def __str__(self):
+        return (f"{super().__str__()}, {self.__weight} kg")
+    
+def main():
+    laptop = LaptopComputer("NoteBook Pro15", 1500, 2)
+    print(laptop)
+if __name__=="__main__":
+    main()
