c8b3029a50eb0bbe3b9a1ab71445414c0e2ff0a1
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-20 08:09:53 +0530

    Part10 Exercise6 Completed

--- part10-06_secret_magic_potion/src/secret_magic_potion.py
+++ part10-06_secret_magic_potion/src/secret_magic_potion.py
@@ -12,4 +12,30 @@ class MagicPotion:
         for ingredient in self._ingredients:
             print(f"{ingredient[0]} {ingredient[1]} grams")
 
+class SecretMagicPotion(MagicPotion):
+    def __init__(self, name, password):
+        super().__init__(name)
+        self.__password=password
 
+    def add_ingredient(self, ingredient: str, amount: float, password: str):
+        if self.__password == password:
+            super().add_ingredient(ingredient, amount)
+        else:
+            raise ValueError
+
+    def print_recipe(self, password):
+            if self.__password==password:
+                super().print_recipe()
+            else:
+                raise ValueError
+
+def main():
+    diminuendo = SecretMagicPotion("Diminuendo maximus", "hocuspocus")
+    diminuendo.add_ingredient("Toadstool", 1.5, "hocuspocus")
+    diminuendo.add_ingredient("Magic sand", 3.0, "hocuspocus")
+    diminuendo.add_ingredient("Frogspawn", 4.0, "hocuspocus")
+    diminuendo.print_recipe("hocuspocus")
+
+    diminuendo.print_recipe("pocushocus") # WRONG password!
+if __name__=="__main__":
+    main()
