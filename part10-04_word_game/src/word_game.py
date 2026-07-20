3a96cb77711689f4a7760f7ee10d1f4f487e46ba
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-20 07:00:30 +0530

    Part10 Exercise4 Completed//Completed Class hierarchies(Covers inheretence,  method overload etc

--- part10-04_word_game/src/word_game.py
+++ part10-04_word_game/src/word_game.py
@@ -31,3 +31,79 @@ class WordGame():
         print(f"player 1: {self.wins1}")
         print(f"player 2: {self.wins2}")
 
+class LongestWord(WordGame):
+    def __init__(self, rounds):
+        super().__init__(rounds)
+    
+    def round_winner(self, player1_word, player2_word):
+        if len(player1_word)>len(player2_word):
+            return 1
+        elif len(player1_word)==len(player2_word):
+            return
+        else:
+            return 2 
+        
+class MostVowels(WordGame):
+    def __init__(self, rounds):
+        super().__init__(rounds)
+
+    def round_winner_my_sol(self, player1_word, player2_word):
+        vowels=['a','e','i','o','u']
+        vowel_count_p1=0
+        vowel_count_p2=0
+        for vowel in vowels:
+            for letter in player1_word:
+                if vowel==letter:
+                    vowel_count_p1+=1
+            for letter in player2_word:
+                if vowel==letter:
+                    vowel_count_p2+=1
+        if vowel_count_p1>vowel_count_p2:
+            return 1
+        elif vowel_count_p1==vowel_count_p2:
+            return 0
+        else:
+            return 2
+            
+#Refactored to make this program modular and less complex
+    def count_vowels(self, word):
+        vowels=['a','e','i','o','u']
+        vowel_count=0
+        for vowel in vowels:
+            for letter in word:
+                if letter in vowels:
+                    vowel_count+=1
+        return vowel_count
+
+    def round_winner(self, player1_word, player2_word):
+        if self.count_vowels(player1_word)>self.count_vowels(player2_word):
+            return 1
+        elif self.count_vowels(player1_word)<self.count_vowels(player2_word):
+            return 2
+        else:
+            return 0
+
+class RockPaperScissors(WordGame):
+    def __init__(self, rounds):
+        super().__init__(rounds)
+
+    def round_winner(self, player1_word, player2_word):
+        word_list=['rock','paper','scissors']
+        possible_pairs=[('rock','paper'),('paper','scissors'),('scissors','rock')]
+        if (player1_word,player2_word) in possible_pairs:
+            return 2
+        elif (player2_word,player1_word) in possible_pairs:
+            return 1
+        elif player1_word in word_list and player2_word not in word_list:
+            return 1
+        elif player2_word in word_list and player1_word not in word_list:
+            return 2
+        else: 
+            return 0
+
+
+def main():
+    p = MostVowels(4)
+    p.play()
+if __name__=="__main__":
+    main()
