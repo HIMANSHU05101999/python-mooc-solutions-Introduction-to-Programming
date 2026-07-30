2a167a4a974c74a5b697c123be8153886b1b6d9d
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-30 20:41:26 +0530

    Part10 Exercise 4 Completed

--- part11-04_lengths/src/lengths.py
+++ part11-04_lengths/src/lengths.py
@@ -1,1 +1,7 @@
 # WRITE YOUR SOLUTION HERE:
+def lengths(listz):
+    return [len(item) for item in listz]
+
+if __name__=="__main__":
+    lists = [[1,2,3,4,5], [324, -1, 31, 7],[]]
+    print(lengths(lists))
