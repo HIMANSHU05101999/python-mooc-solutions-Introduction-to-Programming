e2235efdbd0c52b7f2a724123400380c058451ed
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-30 07:55:05 +0530

    Completed Part11 Exercise2

--- part11-02_rows_of_stars/src/rows_of_stars.py
+++ part11-02_rows_of_stars/src/rows_of_stars.py
@@ -1,1 +1,8 @@
 # WRITE YOUR SOLUTION HERE:
+def rows_of_stars(numbers: list):
+    return [number * "*" for number in numbers]
+
+if __name__=="__main__":
+    l=[1,2,3,4,5]
+    new_l=(rows_of_stars(l))
+    [print(item) for item in new_l]
