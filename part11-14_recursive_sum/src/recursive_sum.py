dc7bbdce6be428ff733f12db84f454c46d1985f4
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-08-07 06:47:06 +0530

    Completed Part11 Exercise 14

--- part11-14_recursive_sum/src/recursive_sum.py
+++ part11-14_recursive_sum/src/recursive_sum.py
@@ -1,1 +1,14 @@
 # WRITE YOUR SOLUTION HERE:
+def recursive_sum(num: int):
+
+    if num<=1:
+        return 1
+
+    return num + recursive_sum(num-1)
+
+if __name__=="__main__":
+    result = recursive_sum(3)
+    print(result)
+
+    print(recursive_sum(5))
+    print(recursive_sum(10))
