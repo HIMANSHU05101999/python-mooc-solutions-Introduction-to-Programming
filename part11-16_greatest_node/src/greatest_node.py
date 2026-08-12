ca33a5403f92a0f6e56ab2f1304ec34809f373a3
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-08-12 16:52:01 +0530

    Part11 Exercise 16 Completed

--- part11-16_greatest_node/src/greatest_node.py
+++ part11-16_greatest_node/src/greatest_node.py
@@ -5,3 +5,32 @@ class Node:
         self.value = value
         self.left_child = left_child
         self.right_child = right_child
+
+def greatest_node(root: Node):
+    if root == None:
+        return
+
+    highest_node=root.value
+
+    if root.left_child:
+        if highest_node<greatest_node(root.left_child):
+            highest_node=greatest_node(root.left_child)
+
+    if root.right_child:
+        if highest_node<greatest_node(root.right_child):
+            highest_node=greatest_node(root.right_child)
+
+    return highest_node
+
+
+if __name__ == "__main__":
+    tree = Node(2)
+
+    tree.left_child = Node(3)
+    tree.left_child.left_child = Node(5)
+    tree.left_child.right_child = Node(8)
+
+    tree.right_child = Node(4)
+    tree.right_child.right_child = Node(11)
+
+    print(greatest_node(tree))
